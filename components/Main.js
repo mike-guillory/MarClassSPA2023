fecc6cbc9070b303e28951289c157c731e1c8a9d
Mike Guillory <webdevmike101@gmail.com>
2023-04-29 09:41:18 -0500

    Converted to SPA

--- components/Main.js
+++ components/Main.js
@@ -1,9 +1,17 @@
 import html from "html-literal";
 import * as views from "./views";
 
-export default () => html`
-  ${views["Home"]()},
-  ${views["Bio"]()},
-  ${views["Pizza"]()},
-  ${views["Order"]()}
-`;
+
+export default (state) => html` ${views[state.view](state)} `;
+
+// export default () => html`
+//   ${views["Home"]()},
+//   ${views["Bio"]()},
+//   ${views["Pizza"]()},
+//   ${views["Order"]()}
+// `;export default () => html`
+//   ${views["Home"]()},
+//   ${views["Bio"]()},
+//   ${views["Pizza"]()},
+//   ${views["Order"]()}
+// `;
