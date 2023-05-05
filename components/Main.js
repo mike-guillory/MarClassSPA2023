bbda51ed10511709f0e2c66d53dd8a3efd715757
Mike Guillory <webdevmike101@gmail.com>
2023-05-04 19:05:09 -0500

    removed dotenv import and config

--- components/Main.js
+++ components/Main.js
@@ -3,15 +3,3 @@ import * as views from "./views";
 
 
 export default (state) => html` ${views[state.view](state)} `;
-
-// export default () => html`
-//   ${views["Home"]()},
-//   ${views["Bio"]()},
-//   ${views["Pizza"]()},
-//   ${views["Order"]()}
-// `;export default () => html`
-//   ${views["Home"]()},
-//   ${views["Bio"]()},
-//   ${views["Pizza"]()},
-//   ${views["Order"]()}
-// `;
